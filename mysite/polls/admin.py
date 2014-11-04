940cb4aed9ba0770ab733b0a96e5332b37c76b14
Alkaid-seven <18345151786@163.com>
2014-11-04 08:55:06 +0800

    mysite

---
+++ mysite/polls/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+              (None,           {'fields':['question_text']}),
+              ('Date information',{'fields':['pub_data'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_data','was_published_recently')
+    list_filter = ['pub_data']
+    search_fields = ['question_text']
+admin.site.register(Question,QuestionAdmin)
---
+++ mysite/polls/models.py
@@ -0,0 +1,26 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_data = models.DateTimeField('date published')
+
+    def __unicode__(self):              
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_data'
+    was_published_recently.boolead = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):            
+        return self.choice_text
